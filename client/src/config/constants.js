f5de496908c771d8b73043286363262cab4b62f5
Niles Tanner <niles.tanner@gmail.com>
2019-09-02 16:30:53 -0600

    working locally

---
+++ client/src/config/constants.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiUrl: "http://localhost:7000/api/",
+  baseUrl: "http://localhost:7000"
+  //  /apiUrl: "http://shortener.muhzi.com/v1/api/",
+  // baseUrl: "http://muhzi.com"
+  // apiUrl: "https://link.nilestanner.com/api/",
+  // baseUrl: "https://link.nilestanner.com"
+};
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 require('dotenv').config();
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+require('./models/UrlShorten');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 const mongoURI = process.env.MONGODB_URI;
-const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
 const connectOptions = {
   keepAlive: true,
   reconnectTries: Number.MAX_VALUE
 };
-mongoose.Promise = global.Promise;
+
 mongoose.connect(mongoURI, connectOptions, (err, db) => {
   if (err) console.log(`Error`, er);
   console.log(`Connected to MongoDB`);
@@ -18,3 +22,20 @@ mongoose.connect(mongoURI, connectOptions, (err, db) => {
 app.listen(PORT, () => {
  console.log(`Server started on port`, PORT);
 });
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Content-type,Accept,x-access-token,X-Key"
+    );
+    if (req.method == "OPTIONS") {
+      res.status(200).end();
+    } else {
+      next();
+    }
+});
+app.use(bodyParser.json());
+
+require("./routes/urlshorten")(app);
