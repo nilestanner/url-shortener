1b46cfc131ba06fb5d7c078baed49a66170310e6
Niles Tanner <ntanner@plaid.com>
2020-06-09 14:36:54 -0600

    removed comment

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,8 +1,6 @@
 module.exports = {
   // apiUrl: "http://localhost:7000/api/",
   // baseUrl: "http://localhost:7000"
-  //  /apiUrl: "http://shortener.muhzi.com/v1/api/",
-  // baseUrl: "http://muhzi.com"
   apiUrl: "https://link.nilestanner.com/api/",
   baseUrl: "https://link.nilestanner.com"
 };
